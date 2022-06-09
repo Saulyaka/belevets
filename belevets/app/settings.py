4f889a53b469d30e6ad15661553eedf75e1d9d08
saulyaka <alla.popova.rus@gmail.com>
2022-06-09 09:41:51 +0100

    smtp

--- belevets/app/settings.py
+++ belevets/app/settings.py
@@ -155,7 +155,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_PORT = '25'
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 MANAGER_EMAIL = os.environ.get('MANAGER_EMAIL')
@@ -181,4 +181,3 @@ MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
 DEFAULT_FILE_STORAGE = 'app.storage_backends.PublicMediaStorage'
 
-#Check workflows
